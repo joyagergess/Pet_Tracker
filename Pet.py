3ed7c95991517a58b4a61d24aad64d31e42ab283
joyagergess <gergessjoya@gmail.com>
2025-07-01 11:36:33 +0300

    Added a method that displays infos about the pet

--- Pet.py
+++ Pet.py
@@ -4,3 +4,9 @@ class Pet :
         self.name=name
         self.species=species
         self.age=age
+
+    def display_info(self):
+       print(f"Pet name : {self.name} , Pet specie: {self.species}, Pet age :{self.age}, Adopted status :{self.adopted}")
+
+pet1 = Pet("Max", "Dog", 3)
+pet1.display_info()
