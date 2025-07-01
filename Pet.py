a9de9760d1fd9676b182ad077c734add026536c9
joyagergess <gergessjoya@gmail.com>
2025-07-01 12:04:24 +0300

    Created 2 methods and 3 Pet objects to verify my work

--- Pet.py
+++ Pet.py
@@ -7,6 +7,24 @@ class Pet :
 
     def display_info(self):
        print(f"Pet name : {self.name} , Pet specie: {self.species}, Pet age :{self.age}, Adopted status :{self.adopted}")
+    
+    def mark_adopted(self):
+        self.adopted=True
+
+    def birthday(self):
+        self.age+=1
 
 pet1 = Pet("Max", "Dog", 3)
+pet2 = Pet("ivan", "Cat", 2)
+pet3 = Pet("samir", "butterfly", 1)
+
 pet1.display_info()
+pet1.birthday()
+pet1.mark_adopted()
+pet1.display_info()
+
+
+pet2.mark_adopted()
+pet2.display_info()
+
+pet3.display_info()
